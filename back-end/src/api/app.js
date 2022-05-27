c87ba660aa17dd10f12929fadbb804262460d7a9
Mariana Saraiva <marianascm8@gmail.com>
2022-05-27 19:47:19 -0300

    feat: add routes and error

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,13 +1,17 @@
 require('dotenv').config();
 
 const express = require('express');
+
 const app = express();
 
 app.use(express.json());
 
-// testing connection 
-app.get('/test', (_req, res) => {
-  return res.json({ message: "Testing connection" });
-});
+const routes = require('../routes');
+
+const error = require('../middlewares/error');
+
+app.use(routes);
+
+app.use(error);
 
 module.exports = app;
