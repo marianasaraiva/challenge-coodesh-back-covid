844f6fb8495bdb0816b20955bec8a1f4695b745a
Mariana Saraiva <marianascm8@gmail.com>
2022-05-28 12:56:13 -0300

    fix: implemented logic service getAllCasesDateCount

--- back-end/src/services/case/getAllCasesDateCount.js
+++ back-end/src/services/case/getAllCasesDateCount.js
@@ -1,20 +1,19 @@
-const { fn, col } = require('sequelize');
 const { Case } = require('../../models');
 
 const getAllCasesDateCount = async (params) => {
   const result = await Case.findAll({
-    attributes: {
-      exclude: ['id', 'num_sequences', 'num_sequences_total', 'perc_sequences'],
-      // include: [[fn('COUNT', col('variant')), 'variant']],
-    },
-    where: {
-      date: params,
-    },
+    attributes : ['location', 'date', 'variant'],  
+    where: { date: params },
     group: ['location', 'variant'],
-    limit: 150,
   });
+  const country = [...new Set(result.map((e) => e.location))];
+  const covid = country.map((location) => ({
+    location,
+    date: params,
+    variant: result.filter((country) => country.location === location).map(e => e.variant),
+  }));
 
-  return result;
+  return covid;
 };
 
 module.exports = getAllCasesDateCount;
